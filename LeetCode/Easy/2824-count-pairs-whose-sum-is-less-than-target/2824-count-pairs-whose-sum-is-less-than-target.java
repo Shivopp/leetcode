a8bc6dffd561f667789626bac637b1730a7fdc48
Shiv Sachan <117806536+Shivopp@users.noreply.github.com>
2026-07-07 09:51:27 +0530

    07-07-2026 2824-count-pairs-whose-sum-is-less-than-target Easy 3 ms (45.78%) 44.1 MB (65.06%)

--- LeetCode/Easy/2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ LeetCode/Easy/2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -1,16 +1,24 @@
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
 
-        int count=0;
-        for(int i=0;i<nums.size()-1;i++){
+int count=0;
+    Collections.sort(nums);
 
-            for(int j=i+1;j<nums.size();j++){
-                if(nums.get(i)+nums.get(j)<target){
-                    count++;
-                }
-            }
+    int left=0;
+    int right=nums.size()-1;
+
+    while(left<right){
+
+        if(nums.get(left)+nums.get(right)<target){
+            count+=right-left;
+        left++;
+        }
+        else{
+            right--;
         }
 
-        return count;
+    }
+       
+       return count;
     }
 }
