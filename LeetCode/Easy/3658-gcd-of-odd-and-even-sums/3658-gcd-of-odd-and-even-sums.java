5572771e2c46160c9f946dc5e063c1acbaae4f7e
Shiv Sachan <117806536+Shivopp@users.noreply.github.com>
2026-07-15 12:47:47 +0530

    07-15-2026 3658-gcd-of-odd-and-even-sums Easy 1 ms (83.11%) 42.6 MB (57.16%)

--- LeetCode/Easy/3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ LeetCode/Easy/3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,20 +1,21 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
 
-        int sumeven=n*(n+1);
-        int sumodd=n*n;
+        // int sumeven=n*(n+1);
+        // int sumodd=n*n;
 
-         int gcd=0;
+        //  int gcd=0;
 
-         int temp=Math.min(sumeven,sumodd);
+        //  int temp=Math.min(sumeven,sumodd);
 
-         while(temp>0){
-            if(sumeven%temp==0 && sumodd%temp==0){
-                gcd=temp;
-                break;
-            }
-            temp--;
-         }
-        return gcd;
+        //  while(temp>0){
+        //     if(sumeven%temp==0 && sumodd%temp==0){
+        //         gcd=temp;
+        //         break;
+        //     }
+        //     temp--;
+        //  }
+        // return gcd;
+        return n;
     }
 }
