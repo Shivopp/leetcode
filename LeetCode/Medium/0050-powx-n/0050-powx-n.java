a6e68db2cc05cdd1802af561450de38f5757b4bf
Shiv Sachan <117806536+Shivopp@users.noreply.github.com>
2026-07-24 22:32:50 +0530

    07-24-2026 0050-powx-n Medium 0 ms (100%) 48 MB (47.77%)

--- LeetCode/Medium/0050-powx-n/0050-powx-n.java
+++ LeetCode/Medium/0050-powx-n/0050-powx-n.java
@@ -1,7 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        double ans=Math.pow(x,n);
-        return ans;
-        
+        double ans=1;
+         long num=n;
+        if(num<0){
+            x=1/x;
+            num=-num;
+        }
+       
+        while(num>0){
+             if ((num & 1) == 1) {   
+                ans *= x;
+            }
+            x*=x;
+            num/=2;
+        }
+       return ans;
     }
 }
