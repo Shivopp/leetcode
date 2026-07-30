93da5bfb8ff7842eadaa3a367cc5448bf413439f
Shiv Sachan <117806536+Shivopp@users.noreply.github.com>
2026-07-30 20:57:29 +0530

    07-30-2026 0080-remove-duplicates-from-sorted-array-ii Medium 0 ms (100%) 48.5 MB (90.63%)

--- LeetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ LeetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -2,25 +2,21 @@ class Solution {
     public int removeDuplicates(int[] nums) {
 
         if (nums.length <= 2)
-            return nums.length;
+          return nums.length;
 
         int write = 1;
         int count = 1;
-
         for (int i = 1; i < nums.length; i++) {
-
-            if (nums[i] == nums[i - 1]) {
-                count++;
+         if (nums[i] == nums[i - 1]) {
+               count++;
             } else {
                 count = 1;
             }
-
             if (count <= 2) {
                 nums[write] = nums[i];
-                write++;
+              write++;
             }
         }
-
-        return write;
+      return write;
     }
 }
